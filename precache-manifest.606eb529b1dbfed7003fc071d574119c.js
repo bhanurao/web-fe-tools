7c36257d8f79a79ce9a1e4c71dc06725437524bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 09:25:44 +0000

    Deploy bhanurao/web-fe-tools to github.com/bhanurao/web-fe-tools.git:gh-pages

---
+++ precache-manifest.606eb529b1dbfed7003fc071d574119c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe495cf1c33714a10f5c5ca93b9b974",
+    "url": "/web-fe-tools/index.html"
+  },
+  {
+    "revision": "64f058f310af1a51389f",
+    "url": "/web-fe-tools/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "86fcbe7c53fafeecaa89",
+    "url": "/web-fe-tools/static/js/2.ca8ac8ba.chunk.js"
+  },
+  {
+    "revision": "64f058f310af1a51389f",
+    "url": "/web-fe-tools/static/js/main.0f8e8b25.chunk.js"
+  },
+  {
+    "revision": "fee9c6074dd1839f256a",
+    "url": "/web-fe-tools/static/js/runtime-main.bfc88d70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/web-fe-tools/static/media/logo.25bf045c.svg"
+  }
+]);
